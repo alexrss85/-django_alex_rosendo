ff9e0c71f4c58d788b60c1e6c0740fcb3b80bb7a
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-01-20 18:50:42 +0100

    clase dilluns

---
+++ ACTIVITAT_13/TIC_BCN_ARS/centre/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+
+    professor = {"name": "Roger", "surname": "Sobrino", "age": "17"}
+    template = loader.get_template('index_centre.html')
+
+    dades = template.render({'nombre': professor["name"], 
+                             'surname': professor["surname"], 
+                             'age': professor["age"]})
+
+    return HttpResponse(dades)
