2b6f0e6776d536a7532ce4f94ec487c6460d9933
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-02-14 19:03:30 +0100

    model_usuari

--- ACTIVITAT_13/TIC_BCN_ARS/autenticacio/models.py
+++ ACTIVITAT_13/TIC_BCN_ARS/autenticacio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Usuari(models.Model):
+    email = models.EmailField(unique=True, max_length=50)  
+    nom = models.CharField(max_length=20) 
+    cognoms = models.CharField(max_length=20)  
+    contrasenya = models.CharField(max_length=15) 
