3e709fa26b47a603900a216ecc578338aaccef61
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-02-14 19:49:59 +0100

    vista_de_login_sense_session

--- ACTIVITAT_13/TIC_BCN_ARS/autenticacio/urls.py
+++ ACTIVITAT_13/TIC_BCN_ARS/autenticacio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loginSense/', views.login_senseSessio, name='login_senseSessio'),
-    path('inici/', views.inici, name='pagina_inici'),  
+    path('login/', views.login_sense_sessio, name='login'),
+    path('inici/', views.inici, name='inici'),  
 ]
--- ACTIVITAT_13/TIC_BCN_ARS/autenticacio/views.py
+++ ACTIVITAT_13/TIC_BCN_ARS/autenticacio/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import LoginForm
+from .models import Usuari
 
-# Create your views here.
-def login_senseSessio(request):
-    return render(request,'login.html')
+def login_sense_sessio(request):
+    error = None 
+    
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            contrasenya = form.cleaned_data["contrasenya"]
+            
+            try:
+                usuari = Usuari.objects.get(email=email, contrasenya=contrasenya)
+                return redirect("inici")
+            except Usuari.DoesNotExist:
+                error = "Credencials incorrectes."
+
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form, "error": error})
 
 def inici(request):
     return render(request,'inici.html')
