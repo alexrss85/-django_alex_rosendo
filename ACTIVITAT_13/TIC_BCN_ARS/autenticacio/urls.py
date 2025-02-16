f1df3b32f3ae52389e4550d0f4b4da62b63ffb6b
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-02-16 23:58:22 +0100

    vista_inici_sessio

--- ACTIVITAT_13/TIC_BCN_ARS/autenticacio/urls.py
+++ ACTIVITAT_13/TIC_BCN_ARS/autenticacio/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login_sense_sessio, name='login'),
     path('inici/', views.inici, name='inici'),  
+    path('login-sessio/', views.login_amb_sessio, name='login_amb_sessio'),
+    path('logout/', views.logout, name='logout'),
 ]
--- ACTIVITAT_13/TIC_BCN_ARS/autenticacio/views.py
+++ ACTIVITAT_13/TIC_BCN_ARS/autenticacio/views.py
@@ -1,5 +1,7 @@
+from django.db import connection
 from django.shortcuts import redirect, render
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
 from .models import Usuari
 
 def login_sense_sessio(request):
@@ -22,5 +24,49 @@ def login_sense_sessio(request):
 
     return render(request, "login.html", {"form": form, "error": error})
 
+
+
+
+
+def login_amb_sessio(request):
+    if request.user.is_authenticated:  
+        return redirect("inici") 
+
+    error = None
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            contrasenya = form.cleaned_data["contrasenya"]
+
+            usuari = authenticate(request, username=email, password=contrasenya)  
+
+            if usuari is not None:
+                login(request, usuari)  
+                return redirect("inici")  
+            else:
+                error = "Credenciales incorrectas."
+
+    else:
+        form = LoginForm()
+
+    return render(request, "autenticacio/login.html", {"form": form, "error": error})
+
+
+
+
+
 def inici(request):
-    return render(request,'inici.html')
+
+    db_name = connection.settings_dict['NAME']
+
+    return render(request, "inici.html", {
+        "usuari": request.user,  
+        "db_name": db_name,     
+    })
+
+
+def logout(request):
+    request.session.flush() 
+    return redirect("login")  
